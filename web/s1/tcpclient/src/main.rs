4ddce7ad22d141c30433bd632075409a7c2b19a0
Alex Zhang <alexzhangyaotian@gmail.com>
2024-03-24 00:14:26 +0800

    feat:tcp connect

--- web/s1/tcpclient/src/main.rs
+++ web/s1/tcpclient/src/main.rs
@@ -1,4 +1,10 @@
 use std::net::TcpStream;
+use std::io::{Read,Write};
+use std::str;
 fn main() {
-    let _stream = TcpStream::connect("localhost:2024").unwrap();
+    let mut stream = TcpStream::connect("localhost:2024").unwrap();
+    stream.write("Hello".as_bytes()).unwrap();
+    let mut buffer = [0;5];
+    stream.read(&mut buffer).unwrap();
+    println!("Response from server:{:?}",str::from_utf8(&buffer).unwrap());
 }
