78123dc7571f23a388a8a2f6391507222569b6c8
alexzhang <alexzhangyaotian@gmail.com>
2024-03-30 17:22:14 +0800

    done:text_editor

--- text_editor/src/main.rs
+++ text_editor/src/main.rs
@@ -1,7 +1,26 @@
-use std::io::{self, Read};
+use crossterm::{
+    event::{read, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode},
+    ExecutableCommand, Result,
+};
+use std::io::{self, stdout, Read};
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b00011111
+}
 fn main() {
+    let _ = enable_raw_mode();
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}",c)
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("{:#b} in B \r", b);
+        } else {
+            println!("{:?} ({}) in C \r", b, c);
+        }
+        if b == to_ctrl_byte('v') {
+            break;
+        }
     }
 }
